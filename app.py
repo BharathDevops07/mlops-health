54126aebabda7422338c409730ca5f997ff789e6
BharathDevops07 <devops8907@gmail.com>
2026-02-05 18:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pickle
 
 app = FastAPI()
+
+class InputData(BaseModel):
+    text: str
 
 model = pickle.load(open("model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
@@ -10,8 +14,8 @@ vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 def home():
     return {"message": "Health ML API running"}
 
-@app.get("/predict")
-def predict(text: str):
-    X = vectorizer.transform([text])
+@app.post("/predict")
+def predict(data: InputData):
+    X = vectorizer.transform([data.text])
     pred = model.predict(X)[0]
     return {"prediction": pred}
